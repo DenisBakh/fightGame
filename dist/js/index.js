58fb17d41f0fc79dceef6f9fdc806dc8db37dc91
DenBakh <den.bah89@gmail.com>
2020-09-02 15:45:52 +0700

    Добавил анимацию по нажатия кнопки attack

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,20 @@
 //=================index.js
 
+$(document).ready(function ($) {
+	$('.footer-button__attack').on('click', function (e) {
+		e.preventDefault();
+
+		const $this = $('.unit_enemys');
+		const $attackBtn = $('.footer-button__attack');
+
+		if ($attackBtn.hasClass('cooldawn')) return;
+
+		$this.addClass('unit_attack');
+		$attackBtn.addClass('cooldawn');
+
+		setTimeout(function () {
+			$this.removeClass('unit_attack');
+			$attackBtn.removeClass('cooldawn');
+		}, 600)
+	});
+})
